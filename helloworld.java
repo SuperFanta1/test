e1b1b17f32b6f89cc64199092ebd32dabe158004
SuperFanta1 <44030578+SuperFanta1@users.noreply.github.com>
2019-04-23 12:44:15 +0800

    修改helloworld.java文件
    第一次使用GitHub修改文件

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class Test{
   public static void main(String[] args){
-    system.out.println("Hello World!")
+    System.out.println("Hello World!")
+    System.out.println("这是第一次修改")
   }
 }
